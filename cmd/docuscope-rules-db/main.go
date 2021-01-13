bc42144fa0078f55b666eb6ceb4e819930ab9b73
Michael Ringenberg <ringenberg@cmu.edu>
2021-01-13 15:29:32 -0500

    Fixes for rules-db.

--- cmd/docuscope-rules-db/main.go
+++ cmd/docuscope-rules-db/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"os"
@@ -11,11 +12,14 @@ import (
 	"runtime"
 	"runtime/pprof"
 	"strings"
+	"sync"
 	
 	"github.com/urfave/cli/v2"
 	"gitlab.com/CMU_Sidecar/docuscope-dictionary-tools/docuscope-rules/internal/pkg/unobfuscate"
 	"gitlab.com/CMU_Sidecar/docuscope-dictionary-tools/docuscope-rules/internal/pkg/fix"
 	"gitlab.com/CMU_Sidecar/docuscope-dictionary-tools/docuscope-rules/internal/pkg/wordclasses"
+
+	"golang.org/x/text/message"
 )
 
 func main() {
@@ -55,6 +59,28 @@ func main() {
 		},
 		Action: func(c *cli.Context) error {
 			return getDictionaryDB(c.Args().First(), flagStats)
+			/*if _, err := os.Stdout.WriteString("["); err != nil {
+				panic(err)
+			}
+			cnt, words, orig, missing, err := Lats(c.Args().First())
+			if err == nil {
+				w, errm := json.Marshal(Words{words})
+				if errm != nil {
+					panic(err)
+				}
+				if _, erro := os.Stdout.Write(w); erro != nil {
+					panic(err)
+				}
+				if _, erro := os.Stdout.WriteString("]"); erro != nil {
+					panic(err)
+				}
+				if flagStats {
+					p := message.NewPrinter(message.MatchLanguage("en"))
+					p.Fprintf(os.Stderr, "Rule Count: %d\n", cnt)
+					p.Fprintf(os.Stderr, "Missing words added: %d; Original: %d; Final: %d\n", missing, orig, len(words))
+				}
+			}
+			return err*/
 		},
 	}
 
@@ -110,6 +136,7 @@ func getDictionaryDB(directory string, flagStats bool) error {
 	words := make(map[string][]string)
 	defaultWordsCount := 0
 	missingWordsCount := 0
+	ruleCount := 0
 	wordclasses.ReadWords(words, filepath.Join(directory, "_wordclasses.txt"))
 	defaultWordsCount = len(words)
 	if _, err := os.Stdout.WriteString("["); err != nil {
@@ -127,6 +154,7 @@ func getDictionaryDB(directory string, flagStats bool) error {
 			lat := strings.TrimSuffix(base, ".txt")
 			content, err := os.Open(filepath.Clean(path))
 			if err != nil {
+				fmt.Printf("Error: unable to access %q: %v\n", path, err)
 				panic(err)
 			}
 			scanner := bufio.NewScanner(content)
@@ -137,10 +165,11 @@ func getDictionaryDB(directory string, flagStats bool) error {
 					if err != nil {
 						panic(err)
 					}
+					ruleCount++
 					if _, err := os.Stdout.Write(b); err != nil {
 						panic(err)
 					}
-					if _, err := os.Stdout.WriteString(","); err != nil {
+					if _, err := os.Stdout.WriteString(",\n"); err != nil {
 						panic(err)
 					}
 					for _, w := range pattern {
@@ -168,7 +197,129 @@ func getDictionaryDB(directory string, flagStats bool) error {
 		panic(err)
 	}
 	if flagStats {
-		fmt.Fprintln(os.Stderr, "Missing words:", defaultWordsCount, missingWordsCount, len(words))
+		p := message.NewPrinter(message.MatchLanguage("en"))
+		p.Fprintf(os.Stderr, "Rule Count: %d\n", ruleCount)
+		p.Fprintf(os.Stderr, "Missing words added: %d; Original: %d; Final: %d\n", missingWordsCount, defaultWordsCount, len(words))
 	}
 	return nil
+}
+
+func walkFiles(done <-chan struct{}, root string) (<-chan string, <-chan error) {
+	paths := make(chan string)
+	errc := make(chan error, 1)
+	go func() {
+		defer close(paths)
+		errc <- filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error: unable to access %q: %v\n", path, err)
+				return err
+			}
+			if !info.Mode().IsRegular() || info.IsDir() ||
+				filepath.Ext(path) != ".txt" ||
+				strings.HasPrefix(filepath.Base(path), "_") {
+				// fmt.Fprintln(os.Stderr, "Ignoring: ", path)
+				return nil
+			}
+			select {
+			case paths <- path:
+			case <-done:
+				return errors.New("File directory walk canceled")
+			}
+			return nil
+		})
+	}()
+	return paths, errc
+}
+
+type result struct {
+	path string
+	lat Rule
+	err error
+}
+
+func ruliser(done <-chan struct{}, paths <-chan string, c chan<- result) {
+	patternRe := regexp.MustCompile(`[!?\w'-]+|[!"#$%&'()*+,-./:;<=>?@[\]^_\` + "`" + `{|}~]`)
+	for path := range paths {
+		base := filepath.Base(path)
+		content, err := os.Open(filepath.Clean(path))
+		if err != nil {
+			select {
+			case c <- result{path, Rule{}, err}:
+			case <-done:
+				return
+			}
+		}
+		scanner := bufio.NewScanner(content)
+		for scanner.Scan() {
+			pattern := fix.Case(patternRe.FindAllString(scanner.Text(), -1))
+			if (len(pattern) > 0) {
+				lat := strings.TrimSuffix(base, ".txt")
+				select {
+				case c <- result{path, Rule{lat, pattern}, err}:
+				case <-done:
+					return
+				}
+			}
+		}
+		if err := content.Close(); err != nil {
+			log.Fatal("Could not close content file: ", err)
+			c <- result{path, Rule{}, err}
+		}
+	}
+}
+
+func Lats(root string) (int, map[string][]string, int, int, error) {
+	words := make(map[string][]string)
+	defaultWordsCount := 0
+	missingWordsCount := 0
+	wordclasses.ReadWords(words, filepath.Join(root, "_wordclasses.txt"))
+	defaultWordsCount = len(words)
+	
+	done := make(chan struct{})
+	defer close(done)
+	paths, errc := walkFiles(done, root)
+
+	c := make(chan result)
+	var wg sync.WaitGroup
+	const numDigesters = 30
+	wg.Add(numDigesters)
+	for i := 0; i < numDigesters; i++ {
+		go func() {
+			ruliser(done, paths, c)
+			wg.Done()
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(c)
+	}()
+
+	ruleCount := 0
+	for r := range c {
+		if r.err != nil {
+			return ruleCount, words, defaultWordsCount, missingWordsCount, r.err
+		}
+		// Add any missing words
+		for _, w := range r.lat.Pat {
+			if wds, ok := words[w]; !ok {
+				words[w] = append(wds, w)
+				missingWordsCount++
+			}
+		}
+		b, err := json.Marshal(r.lat)
+		if err != nil {
+			return ruleCount, words, defaultWordsCount, missingWordsCount, err
+		}
+		ruleCount += 1
+		if _, err := os.Stdout.Write(b); err != nil {
+			return ruleCount, words, defaultWordsCount, missingWordsCount, err
+		}
+		if _, err := os.Stdout.WriteString(",\n"); err != nil {
+			return ruleCount, words, defaultWordsCount, missingWordsCount, err
+		}
+	}
+	if err := <-errc; err != nil {
+		return ruleCount, words, defaultWordsCount, missingWordsCount, err
+	}
+	return ruleCount, words, defaultWordsCount, missingWordsCount, nil
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module gitlab.com/CMU_Sidecar/docuscope-dictionary-tools/docuscope-rules
 
 go 1.13
 
-require github.com/urfave/cli/v2 v2.2.0
+require (
+	github.com/urfave/cli/v2 v2.2.0
+	golang.org/x/text v0.3.3
+)
